236633c76d773e072a8ff4c94d9b2794a68cc177
Rich <ritasker8t2@gmail.com>
2013-07-28 20:41:25 +0100

    made script neater.

--- Chocolatey-Setup-Env.ps1
+++ Chocolatey-Setup-Env.ps1
@@ -1,36 +1,43 @@
-"Installing Chocolatey..."
 # Install Chocolatey
+
+"Installing Chocolatey..."
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
+"Installing Chocolatey - Done..."
 
 # Install Programes
 
   # System Tools
+  
   "Installing System Tools..."
-  cinst 7zip 
+  
+  cinst 7zip
   cinst notepadplusplus
   cinst GoogleChrome
   cinst Firefox
-  cinst filezilla 
-  cinst skype 
-  cinst FoxitReader 
-  cinst gimp 
-  cinst keepass 
-  cinst truecrypt 
-  cinst CutePDF 
-  cinst mirc 
-  cinst putty 
-  cinst Cygwin 
+  cinst filezilla
+  cinst skype
+  cinst FoxitReader
+  cinst gimp
+  cinst keepass
+  cinst truecrypt
+  cinst CutePDF
+  cinst mirc
+  cinst putty
+  cinst Cygwin
   cinst virtualbox
+  
   "Installing System Tools - Done..."
   
   # Dev Tools
+  
   "Installing Dev Tools..."
-  cinst git 
-  cinst poshgit 
-  cinst P4Merge 
-  cinst fiddler 
-  cinst linqpad4 
-  cinst ScriptCs 
-  cinst CoffeeScript 
-  cinst NewRelic
+  
+  cinst fiddler
+  cinst linqpad4
+  cinst ScriptCs
+  cinst CoffeeScript
+  cinst NewRelic  
+  cinst P4Merge
+  cinst poshgit
+  
   "Installing Dev Tools - Done..."
