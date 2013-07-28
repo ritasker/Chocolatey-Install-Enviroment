39d154632602c690927e2b74d97ca21c9d2d1635
Rich <ritasker8t2@gmail.com>
2013-07-28 20:22:59 +0100

    adds the packages to install.

--- Chocolatey-Setup-Env.ps1
+++ Chocolatey-Setup-Env.ps1
@@ -1,5 +1,36 @@
+"Installing Chocolatey..."
 # Install Chocolatey
-Set-ExecutionPolicy Unrestricted
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 
 # Install Programes
+
+  # System Tools
+  "Installing System Tools..."
+  cinst 7zip 
+  cinst notepadplusplus
+  cinst GoogleChrome
+  cinst Firefox
+  cinst filezilla 
+  cinst skype 
+  cinst FoxitReader 
+  cinst gimp 
+  cinst keepass 
+  cinst truecrypt 
+  cinst CutePDF 
+  cinst mirc 
+  cinst putty 
+  cinst Cygwin 
+  cinst virtualbox
+  "Installing System Tools - Done..."
+  
+  # Dev Tools
+  "Installing Dev Tools..."
+  cinst git 
+  cinst poshgit 
+  cinst P4Merge 
+  cinst fiddler 
+  cinst linqpad4 
+  cinst ScriptCs 
+  cinst CoffeeScript 
+  cinst NewRelic
+  "Installing Dev Tools - Done..."
